5ab365fee419ea27fe5ba8f8b7b15fdb34e4a8e2
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-03-25 22:51:38 +0100

    Scrolling page works + style fixes

--- presentation.js
+++ presentation.js
@@ -7,7 +7,7 @@ let tl = gsap.timeline({
         scrub: true,
         markers: true,
         start: "top 95%",
-        end: "top 5%"
+        end: "bottom 5%"
     }
 })
 
@@ -17,7 +17,7 @@ tl.to("#my-paragraph, #my-img", {
 })
 .to("#my-paragraph, #my-img", {
     opacity: 1,
-    duration: 2
+    duration: 1
 })
 .to("#my-paragraph, #my-img", {
     scale: 1.1,
@@ -25,41 +25,114 @@ tl.to("#my-paragraph, #my-img", {
 })
 .to("#my-paragraph, #my-img", {
     opacity: 0,
-    duration: 2
+    duration: 1
 })
 
 
-// tl.to("#my-img", {
-//     opacity: 1,
-//     duration: 1
-// })
-// .to("#my-img", {
-//     scale: 1.1,
-//     duration: 5
-// })
-// .to("#my-img", {
-//     opacity: 0,
-//     duration: 1
-// })
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#the_section2",
+        scrub: true,
+        markers: true,
+        start: "top 95%",
+        end: "bottom 5%"
+    }
+})
 
-// let tl2 = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: "#the_section2",
-//         scrub: true,
-//         markers: false,
-//         start: "top 85%",
-//         end: "top 15%"
-//     }
-// })
-// tl2.to("#my-img2", {
-//     opacity: 1,
-//     duration: 2
-// })
-// .to("#my-img2", {
-//     scale: 1.1,
-//     duration: 5
-// })
-// .to("#my-img2", {
-//     opacity: 0,
-//     duration: 2
-// })
+tl2.to("#my-paragraph2, #my-img2", {
+    opacity: 0,
+    duration: 0
+})
+.to("#my-paragraph2, #my-img2", {
+    opacity: 1,
+    duration: 1
+})
+.to("#my-paragraph2, #my-img2", {
+    scale: 1.1,
+    duration: 5
+})
+.to("#my-paragraph2, #my-img2", {
+    opacity: 0,
+    duration: 1
+})
+
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#the_section3",
+        scrub: true,
+        markers: true,
+        start: "top 95%",
+        end: "bottom 5%"
+    }
+})
+
+tl3.to("#my-paragraph3, #my-img3", {
+    opacity: 0,
+    duration: 0
+})
+.to("#my-paragraph3, #my-img3", {
+    opacity: 1,
+    duration: 1
+})
+.to("#my-paragraph3, #my-img3", {
+    scale: 1.1,
+    duration: 5
+})
+.to("#my-paragraph3, #my-img3", {
+    opacity: 0,
+    duration: 1
+})
+
+let tl4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#the_section4",
+        scrub: true,
+        markers: true,
+        start: "top 95%",
+        end: "bottom 5%"
+    }
+})
+
+tl4.to("#my-paragraph4, #my-img4", {
+    opacity: 0,
+    duration: 0
+})
+.to("#my-paragraph4, #my-img4", {
+    opacity: 1,
+    duration: 1
+})
+.to("#my-paragraph4, #my-img4", {
+    scale: 1.1,
+    duration: 5
+})
+.to("#my-paragraph4, #my-img4", {
+    opacity: 0,
+    duration: 1
+})
+
+let tl5 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#the_section5",
+        scrub: true,
+        markers: true,
+        start: "top 95%",
+        end: "bottom 5%"
+    }
+})
+
+tl5.to("#my-paragraph5, #my-img5", {
+    opacity: 0,
+    duration: 0
+})
+.to("#my-paragraph5, #my-img5", {
+    opacity: 1,
+    duration: 1
+})
+.to("#my-paragraph5, #my-img5", {
+    scale: 1.1,
+    duration: 5
+})
+.to("#my-paragraph5, #my-img5", {
+    opacity: 0,
+    duration: 1
+})
